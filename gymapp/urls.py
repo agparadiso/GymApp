e367524ed37c918892ce744e729418ac7defee9d
Gabriel Paradiso <gabriel.paradiso@typeform.com>
2018-02-05 21:57:45 +0100

    added logout view

--- gymapp/urls.py
+++ gymapp/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', auth_views.login, name='login'),
+    path('logout', auth_views.logout, {'next_page': '/'}, name='logout'),
     path('home/', admin.site.urls), #homeView
 ]
